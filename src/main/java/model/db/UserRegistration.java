018a3a7022bc3ac4bbc81c5e8dc9496f78ad240c
acil <acil@acil-VirtualBox>
2017-10-17 23:56:01 -0230

    UserRegistration

--- src/main/java/model/db/UserRegistration.java
+++ src/main/java/model/db/UserRegistration.java
@@ -16,11 +16,12 @@ public class UserRegistration extends User {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void userRegistration(String regUserName, String regUserPassword, String regFirstName , String regLastName, String regEmail , String regDateOfBirth, String regOrganization, String regJob)
+	//public void userRegistration(String regUserName, String regUserPassword, String regFirstName , String regLastName, String regEmail , String regDateOfBirth, String regOrganization, String regJob)
+	public void userRegistration(User user)
 	{
 		// create new user in the database
-		String userRegQueryStatement = "insert into" + tableName + "(first_name, last_name, username, password, email,DOB, organization, job, regdate) values ('" + getUserName() + "','" 
-			    + getPassword() + "','" + getFirstName() + "','" + getLastName() + "','" + getEmail() + "','" + getDateOfBirth() + "','" + getOrganization() + "','" + getJob() + "', CURDATE())";
+		String userRegQueryStatement = "insert into" + tableName + "(first_name, last_name, username, password, email,DOB, organization, job, regdate) values ('" + user.getUserName() + "','" 
+			    + user.getPassword() + "','" + user.getFirstName() + "','" + user.getLastName() + "','" + user.getEmail() + "','" + user.getDateOfBirth() + "','" + user.getOrganization() + "','" + user.getJob() + "', CURDATE())";
 		//DBConnection code
 		DBConnection userRegDBConnection =new DBConnection(); 
 		userRegDBConnection.processDataToDB(userRegQueryStatement);		
